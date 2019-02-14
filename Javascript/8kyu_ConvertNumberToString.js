0126c45ebb2fcf5da613dacc346a182348ded121
mmyenny <mmy491@gmail.com>
2019-02-14 14:34:25 -0500

    Convert number to string kata completed

---
+++ Javascript/8kyu_ConvertNumberToString.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/convert-a-number-to-a-string/train/javascript
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// numberToString(123); // returns '123';`
+// numberToString(999); // returns '999';`
+
+function numberToString(num) {
+  return num.toString()
+}

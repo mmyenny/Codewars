a1ec0862a5fded63dd7a287c13aa8fd26f4b59bb
mmyenny <mmy491@gmail.com>
2019-02-19 18:39:33 -0500

    Remove exclamation marks kata completed

---
+++ Javascript/8kyu_RemoveExclamationMarks.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/remove-exclamation-marks/train/javascript
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+  return s.replace(/!/g, '')
+}

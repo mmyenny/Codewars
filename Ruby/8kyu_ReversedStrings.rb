213168a0dc7248382687307091672ff9dae804b1
mmyenny <mmy491@gmail.com>
2019-02-21 12:49:52 -0500

    Reverse string kata completed in JS and Ruby

---
+++ Ruby/8kyu_ReversedStrings.rb
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/reversed-strings/train/ruby
+
+# Complete the solution so that it reverses the string value passed into it.
+
+# solution('world') # returns 'dlrow'
+
+def solution(str)
+  str.reverse!
+end

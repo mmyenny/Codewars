e736478621bf9162b933d26ab3733fca8ee46dc8
mmyenny <mmy491@gmail.com>
2019-03-04 22:44:47 -0500

    Easy SQL: LowerCase kata completed

---
+++ SQL/8kyu_EasySQLLowerCase.sql
@@ -0,0 +1,13 @@
+-- https://www.codewars.com/kata/easy-sql-lowercase/train/sql
+
+-- Given a demographics table in the following format:
+
+-- ** demographics table schema **
+
+-- id
+-- name
+-- birthday
+-- race
+-- you need to return the same table where all letters are lowercase in the race column.
+
+SELECT *, LOWER(race) AS race FROM demographics

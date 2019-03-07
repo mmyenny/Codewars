e0a9ad486563617253a1f3eceada731c969610ad
mmyenny <mmy491@gmail.com>
2019-03-06 23:37:01 -0500

    Basic mod kata completed

---
+++ SQL/8kyu_SQLBasicsMod.sql
@@ -0,0 +1,12 @@
+-- https://www.codewars.com/kata/sql-basics-mod/train/sql
+
+-- Given the following table 'decimals':
+
+-- ** decimals table schema **
+
+-- id
+-- number1
+-- number2
+-- Return a table with one column (mod) which is the output of number1 modulus number2.
+
+SELECT number1 % number2 AS mod FROM decimals

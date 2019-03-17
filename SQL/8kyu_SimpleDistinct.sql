6698907416cdd3c57c0227cb68630874a6ce641f
mmyenny <mmy491@gmail.com>
2019-03-16 21:37:24 -0400

    SQL Basics: Simple DISTINCT kata completed

---
+++ SQL/8kyu_SimpleDistinct.sql
@@ -0,0 +1,12 @@
+-- https://www.codewars.com/kata/sql-basics-simple-distinct/train/sql
+
+-- For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+
+-- people table schema
+-- id
+-- name
+-- age
+-- select table schema
+-- age (distinct)
+
+SELECT DISTINCT(age) AS age FROM people

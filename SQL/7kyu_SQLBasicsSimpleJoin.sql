15c63cc035e1106260755d8db547ba90e9319f69
mmyenny <mmy491@gmail.com>
2019-03-19 21:16:54 -0400

    SQL Basics: Simple JOIN kata completed

---
+++ SQL/7kyu_SQLBasicsSimpleJoin.sql
@@ -0,0 +1,17 @@
+-- https://www.codewars.com/kata/sql-basics-simple-join/train/sql
+
+-- For this challenge you need to create a simple SELECT statement that will return all columns from the products table, and join to the companies table so that you can return the company name.
+
+-- products table schema
+-- id
+-- name
+-- isbn
+-- company_id
+-- price
+-- companies table schema
+-- id
+-- name
+-- You should return all product fields as well as the company name as "company_name".
+
+SELECT products.*, companies.name AS company_name 
+FROM products JOIN companies ON products.company_id = companies.id

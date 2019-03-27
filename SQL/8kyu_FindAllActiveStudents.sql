0bc9a7f973284c0b3bf9fd74c876ebbc73f3d560
mmyenny <mmy491@gmail.com>
2019-03-27 19:53:56 -0400

    Find all active students Kata completed

---
+++ SQL/8kyu_FindAllActiveStudents.sql
@@ -0,0 +1,12 @@
+-- https://www.codewars.com/kata/5809b9ef88b750ab180001ec
+
+-- Create a simple SELECT query to display student information of all ACTIVE students.
+
+-- TABLE STRUCTURE:
+
+-- students
+-- Id	FirstName	LastName	IsActive
+
+-- Note: IsActive (true or false)
+
+SELECT * FROM students WHERE IsActive

d7db0e63b9260beb991b36fefcc66cdd5e5736c7
mmyenny <mmy491@gmail.com>
2019-04-04 22:48:19 -0400

    Parse nice int from char problem kata completed

--- Javascript/7kyu_FizzBuzz.js
+++ Javascript/7kyu_FizzBuzz.js
@@ -20,3 +20,21 @@ function fizzbuzz(n) {
   }
   return arr
 }
+
+// OR
+
+function fizzbuzz(n) {
+  let array = []
+  for (i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      array.push('FizzBuzz')
+    } else if (i % 3 === 0) {
+      array.push('Fizz')
+    } else if (i % 5 === 0) {
+      array.push('Buzz')
+    } else {
+      array.push(i)
+    }
+  }
+  return array
+}

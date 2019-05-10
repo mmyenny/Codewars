15ab50579d444980602f739cf60bd41bf0237a76
mmyenny <mmy491@gmail.com>
2019-05-09 22:10:11 -0400

    Multiply kata completed in java

---
+++ Java/Multiply.java
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/multiply/train/java
+
+// The code does not execute properly. Try to figure out why.
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}

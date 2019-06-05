d38dab90e2db0c1cae95a80a0fb647d733357c7a
mmyenny <mmy491@gmail.com>
2019-06-04 23:34:08 -0400

    Convert a String to a Number! and Beginner Series #1 School Paperwork katas completed

---
+++ Ruby/8kyu_ConvertStringToNumber.rb
@@ -0,0 +1,18 @@
+# https://www.codewars.com/kata/convert-a-string-to-a-number/train/ruby
+
+# Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+# Description
+# We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+# Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+# Examples
+# string_to_number "1234"  == 1234
+# string_to_number "605"   == 605
+# string_to_number "1405"  == 1405
+# string_to_number "-7"    == -7
+
+def string_to_number(s)
+  s.to_i
+end

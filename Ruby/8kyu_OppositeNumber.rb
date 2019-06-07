2b2eb32ba8f9aa80e0d408dd4da0356b7c67e1cb
mmyenny <mmy491@gmail.com>
2019-06-06 23:49:13 -0400

    Opposite number and Sentence Smash katas completed

---
+++ Ruby/8kyu_OppositeNumber.rb
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/opposite-number/ruby
+
+# Very simple, given a number, find its opposite.
+
+# Examples:
+
+# 1: -1
+# 14: -14
+# -34: 34
+
+def opposite num
+  -num
+ end
